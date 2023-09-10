dfa254e88ff64b39d519a17016badd101c613b5b
sainath13 <latkarsainath@gmail.com>
2023-09-10 18:21:51 +0530

    Add progress loading screen

--- script.js
+++ script.js
@@ -15,52 +15,69 @@ chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
     let url = tabs[0].url;
     console.log(url)
     if(url.startsWith('https://app.hellosign.com/')){
-        const jokeElement = document.getElementById('summary');
-        jokeElement.innerHTML = "YESSSSS"
+        const summaryElement = document.getElementById('summary');
+        summaryElement.innerHTML = "YESSSSS"
     }
     else{
-        const jokeElement = document.getElementById('summary');
-        jokeElement.innerHTML = "NOOOOOOOO"
+        const summaryElement = document.getElementById('summary');
+        summaryElement.innerHTML = "NOOOOOOOO"
     }
     fetch('http://0.0.0.0:8000/default/0a9ad55670de6a3efc65d855ac9ed885b5198997')
         .then(data => data.json())
         .then(result => {
             const summary = result.summary;
             summaryElement.innerHTML = summary;
-        })
-    
+            const tagHolder = document.getElementById('tagHolder');
+
+            result.originalTexts.forEach(text => {
+                console.log("creating tag for {}", text.title);
+                const tagControl = document.createElement('div');
+                tagControl.className = 'control';
+
+                const tagDiv = document.createElement('div');
+                tagDiv.className = 'tags has-addons';
+
+                const tagLink = document.createElement('a');
+                tagLink.className = 'tag is-link';
+                tagLink.setAttribute('data-tag-details', text.title); // Use title as tag details
+
+                tagLink.textContent = text.title; // Set tag text
+
+                tagDiv.appendChild(tagLink);
+                tagControl.appendChild(tagDiv);
+
+                tagHolder.appendChild(tagControl);
+            });
+        
+            const tagLinks = document.querySelectorAll('.tag.is-link');
+            console.log("tag links is {}", tagLinks)
+            tagLinks.forEach(tagLink => {
+                tagLink.addEventListener('click', () => {
+                    console.log("cliked on a tag link");
+                    content.style.display = 'none';
+                    const tagDetails = tagLink.getAttribute('data-tag-details');
+                
+                    tagDescription.textContent = tagDetails;
+                    tagHolder.style.display = 'block'
+                    document.getElementById('tagDetails').style.display = 'block';
+                    document.getElementById('tagDescriptionName').text = tagDetails;
+                    
+                });
+            });
+            
+            returnToAllTags.addEventListener('click', () => {
+                console.log("cliked on return all");
+                content.style.display = 'block';
+                document.getElementById('tagDetails').style.display = 'none';
+                
+            });
+    })
 });
 
-const tagLinks = document.querySelectorAll('.tag.is-link');
 const tagDescription = document.getElementById('tagDescription');
 const returnToAllTags = document.getElementById('returnToAllTags');
 
-tagLinks.forEach(tagLink => {
-    tagLink.addEventListener('click', () => {
-        content.style.display = 'none';
-        const tagDetails = tagLink.getAttribute('data-tag-details');
-        // Set the tag description content
-        tagDescription.textContent = tagDetails;
-        // Show the tag details and the "Return to All Tags" button
-        document.getElementById('tagDetails').style.display = 'block';
-        // returnToAllTags.style.display = 'block';
-        // Hide the delete button for the clicked tag
-        // tagLink.nextElementSibling.style.display = 'none';
-        
-    });
-});
 
-// JavaScript for returning to all tags
-returnToAllTags.addEventListener('click', () => {
-    // Show all tags
-    // tagLinks.forEach(tag => tag.nextElementSibling.style.display = 'block');
-    // Hide the tag details and the "Return to All Tags" button
-    // document.getElementById('tagDetails').style.display = 'none';
-    // returnToAllTags.style.display = 'none';
-    content.style.display = 'block';
-    document.getElementById('tagDetails').style.display = 'none';
-    
-});
 
 button.addEventListener('click', () => {
     if (content.style.display === 'none' || content.style.display === '') {
