e896d32aefd5edf80f9851ea390b32548b30f82e
sainath13 <latkarsainath@gmail.com>
2023-09-10 18:48:49 +0530

    add loading bar

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const summaryElement = document.getElementById('summary');
 const button = document.getElementById('legalSimplificationButton');
 const content = document.getElementById('legalSimplificationContent');
 const summaryPage = document.getElementById("summaryPage");
+const loadingScreen = document.getElementById('loadingScreen');
 
 chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
     let url = tabs[0].url;
@@ -22,30 +23,27 @@ chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
         const summaryElement = document.getElementById('summary');
         summaryElement.innerHTML = "NOOOOOOOO"
     }
+    loadingScreen.style.display = 'block';
     fetch('http://0.0.0.0:8000/default/0a9ad55670de6a3efc65d855ac9ed885b5198997')
         .then(data => data.json())
         .then(result => {
             const summary = result.summary;
             summaryElement.innerHTML = summary;
-            const tagHolder = document.getElementById('tagHolder');
 
+            loadingScreen.style.display = 'none';
+            const tagHolder = document.getElementById('tagHolder');
             result.originalTexts.forEach(text => {
                 console.log("creating tag for {}", text.title);
                 const tagControl = document.createElement('div');
                 tagControl.className = 'control';
-
                 const tagDiv = document.createElement('div');
                 tagDiv.className = 'tags has-addons';
-
                 const tagLink = document.createElement('a');
                 tagLink.className = 'tag is-link';
-                tagLink.setAttribute('data-tag-details', text.title); // Use title as tag details
-
-                tagLink.textContent = text.title; // Set tag text
-
+                tagLink.setAttribute('data-tag-details', text.title);
+                tagLink.textContent = text.title;
                 tagDiv.appendChild(tagLink);
                 tagControl.appendChild(tagDiv);
-
                 tagHolder.appendChild(tagControl);
             });
         
